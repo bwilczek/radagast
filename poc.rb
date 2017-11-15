6ff504fda7256480b3e41e9084421f39cb69c81f
Bartek Wilczek <bwilczek@gmail.com>
2017-11-15 20:23:23 +0100

    [WiP] Handling SIGINT

--- poc.rb
+++ poc.rb
@@ -16,43 +16,59 @@ connection = {
   pass: 'guest'
 }
 
-puts "Setup..."
-@rabbit = Bunny.new connection
-@rabbit.start
+begin
+  puts "Setup..."
+  @rabbit = Bunny.new connection
+  @rabbit.start
 
-@channel = @rabbit.create_channel
-@queue_tasks  = @channel.queue('tasks', :auto_delete => true)
-@queue_results  = @channel.queue('results', :auto_delete => true)
+  @channel = @rabbit.create_channel
+  @queue_tasks  = @channel.queue('tasks', :auto_delete => true)
+  @queue_results  = @channel.queue('results', :auto_delete => true)
 
-########## MANAGER ################
-if options[:manager]
-  puts "Start manager..."
-  @exchange_tasks = @channel.default_exchange
+  ########## MANAGER ################
+  if options[:manager]
+    puts "Start manager..."
+    @exchange_tasks = @channel.default_exchange
 
-  # publish some example tasks
-  puts "Push some tasks..."
-  @exchange_tasks.publish(JSON.generate(cmd: 'pwd'), :routing_key => 'tasks')
-  @exchange_tasks.publish(JSON.generate(cmd: 'whoami'), :routing_key => 'tasks')
-  @exchange_tasks.publish(JSON.generate(cmd: 'ls'), :routing_key => 'tasks')
+    # master subscribes to results queue
+    t = Thread.new do
+      puts "Subscribe for results..."
+      @queue_results.subscribe(block: true) do |delivery_info, metadata, payload|
+        data = JSON.parse(payload)
+        puts "Result has arrived: #{data}"
+      end
+    end
 
-  # master subscribes to results queue
-  puts "Subscribe for results..."
-  @queue_results.subscribe(block: true) do |delivery_info, metadata, payload|
-    data = JSON.parse(payload)
-    puts "Result has arrived: #{data}"
+    # publish some example tasks
+    puts "Push some tasks..."
+    @exchange_tasks.publish(JSON.generate(cmd: 'pwd 1'), :routing_key => 'tasks')
+    @exchange_tasks.publish(JSON.generate(cmd: 'whoami 1'), :routing_key => 'tasks')
+    @exchange_tasks.publish(JSON.generate(cmd: 'ls 1'), :routing_key => 'tasks')
+    @exchange_tasks.publish(JSON.generate(cmd: 'pwd 2'), :routing_key => 'tasks')
+    @exchange_tasks.publish(JSON.generate(cmd: 'whoami 2'), :routing_key => 'tasks')
+    @exchange_tasks.publish(JSON.generate(cmd: 'ls 2'), :routing_key => 'tasks')
+    @exchange_tasks.publish(JSON.generate(cmd: 'pwd 3'), :routing_key => 'tasks')
+    @exchange_tasks.publish(JSON.generate(cmd: 'whoami 3'), :routing_key => 'tasks')
+    @exchange_tasks.publish(JSON.generate(cmd: 'ls 3'), :routing_key => 'tasks')
+
+    t.join
   end
 
-end
+  ########## WORKER ################
+  if options[:worker]
+    puts "Start worker..."
+    worker_id = rand(100..999)
+    @exchange_results = @channel.default_exchange
 
-########## WORKER ################
-if options[:worker]
-  puts "Start worker..."
-  @exchange_results = @channel.default_exchange
-
-  @queue_tasks.subscribe(block: true) do |delivery_info, metadata, payload|
-    data = JSON.parse(payload)
-    puts "Task has arrived: #{data}"
-    # do the job and respond with result
-    @exchange_results.publish(JSON.generate(res: "Result for #{data['cmd']}"), :routing_key => 'results')
+    @queue_tasks.subscribe(block: true) do |delivery_info, metadata, payload|
+      data = JSON.parse(payload)
+      puts "Task has arrived: #{data}"
+      # do the job and respond with result
+      sleep 2
+      @exchange_results.publish(JSON.generate(res: "#{worker_id} Result for #{data['cmd']}"), :routing_key => 'results')
+    end
   end
+rescue SystemExit, Interrupt
+  puts 'Closing RabbitMQ connection...'
+  @rabbit.close
 end
