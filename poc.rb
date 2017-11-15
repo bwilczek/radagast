d6b4081e246f8beda33b7de23c6e8698f8e2c0c9
Bartek Wilczek <bwilczek@gmail.com>
2017-11-15 22:22:36 +0100

    Saving results from call commands

--- poc.rb
+++ poc.rb
@@ -32,6 +32,7 @@ begin
     @exchange_tasks = @channel.default_exchange
     @published_cnt = 0
     @processed_cnt = 0
+    @all_results = []
 
     # master subscribes to results queue
     t = Thread.new do
@@ -39,10 +40,12 @@ begin
       @queue_results.subscribe(block: true) do |delivery_info, metadata, payload|
         @processed_cnt += 1
         data = JSON.parse(payload)
+        @all_results << data
         puts "Result #{@processed_cnt}/#{@published_cnt} has arrived: #{data}"
         if @processed_cnt == @published_cnt
           puts ""
-          puts "Aggregate the results"
+          puts "Aggregate the results (size: #{@all_results.length})"
+          puts " > with non-zero exit code: #{@all_results.count {|i| i['exitstatus'] != 0}}"
           puts "... and exit"
           @rabbit.close
         end
@@ -55,7 +58,7 @@ begin
     @published_cnt += 1
     @exchange_tasks.publish(JSON.generate(cmd: 'id'), :routing_key => 'tasks')
     @published_cnt += 1
-    @exchange_tasks.publish(JSON.generate(cmd: 'ls'), :routing_key => 'tasks')
+    @exchange_tasks.publish(JSON.generate(cmd: 'cat /etc/shadow'), :routing_key => 'tasks')
     @published_cnt += 1
     @exchange_tasks.publish(JSON.generate(cmd: 'cat /etc/passwd | grep bwilczek'), :routing_key => 'tasks')
     @published_cnt += 1
