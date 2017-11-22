51bb19492f5be3e88758d3688a388ab616bf1bcb
Bartek Wilczek <bwilczek@gmail.com>
2017-11-22 12:32:58 +0100

    Refactoring of Result class, unit test added

--- lib/radagast/result.rb
+++ lib/radagast/result.rb
@@ -1,13 +1,13 @@
 module Radagast
   Result = Struct.new(:exit_code, :stdout, :stderr, :meta, :task_id) do
     def self.from_hash(data)
-      o = new
-      o.exit_code = data['exit_code']
-      o.stderr = data['stderr']
-      o.stdout = data['stdout']
-      o.meta = data['meta']
-      o.task_id = data['task_id']
-      o
+      new(
+        data['exit_code'],
+        data['stdout'],
+        data['stderr'],
+        data['meta'],
+        data['task_id']
+      )
     end
   end
 end
---
+++ spec/unit/result_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../../lib/radagast/result.rb'
+
+RSpec.describe Radagast::Result do
+  specify '.from_hash' do
+    data = {
+      'exit_code' => 6,
+      'stderr' => 'err',
+      'stdout' => 'out',
+      'meta' => { k1: :v1, k2: :v2 },
+      'task_id' => 123
+    }
+    result = Radagast::Result.from_hash data
+    expect(result.exit_code).to eq(6)
+    expect(result.stderr).to eq('err')
+    expect(result.stdout).to eq('out')
+    expect(result.meta).to include(k1: :v1, k2: :v2)
+    expect(result.task_id).to eq(123)
+  end
+end
